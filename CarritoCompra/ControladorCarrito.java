f987626d7e67bfe49c6f3deca86b1ea9f7249428
Israel Royano Oro <israelroyanooro@gmail.com>
2025-11-14 14:15:15 +0100

    Add files via upload

--- CarritoCompra/ControladorCarrito.java
+++ CarritoCompra/ControladorCarrito.java
@@ -3,6 +3,8 @@ package goya.daw2.CarritoCompra;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorCarrito {
@@ -22,7 +24,19 @@ public class ControladorCarrito {
 		
 		return "index";
 	}
-	
+	@PostMapping("/")
+	public String principalPost(@RequestParam(name="manzanas", required=false)Integer manzanas,
+			@RequestParam(name="melones", required=false)Integer melones,
+			@RequestParam(name="peras", required=false)Integer peras, Model modelo) {
+		
+		if (manzanas != null) stock.modificar("manzanas", stock.getAll().get("manzanas")-manzanas);
+		
+		if (peras != null) stock.modificar("peras", stock.getAll().get("peras")-peras);
+		
+		if (melones != null) stock.modificar("melones", stock.getAll().get("melones")-melones);
+		
+		return "index";
+	}
 	
 	
 }
