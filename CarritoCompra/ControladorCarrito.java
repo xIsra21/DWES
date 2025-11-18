7512332f5b7e82372c79031b922a13443ef3b44c
Israel Royano Oro <israelroyanooro@gmail.com>
2025-11-18 09:56:15 +0100

    Add files via upload

--- CarritoCompra/ControladorCarrito.java
+++ CarritoCompra/ControladorCarrito.java
@@ -1,23 +1,29 @@
 package goya.daw2.CarritoCompra;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class ControladorCarrito {
-	
+	Map<String, Integer> pedidos = new HashMap<String, Integer>();
 	static RepositorioStock stock=new RepositorioStock();	
 	public static void addStock() {
-		stock.anadir("Manzanas", 5);
+		stock.anadir("Manzanas", 15);
 		stock.anadir("Peras", 5);
 		stock.anadir("Melones", 5);
 		stock.guardar();
 	}
 	@GetMapping("/")
-	public String principal(Model modelo) {
+	public String principal(Model modelo, HttpSession sesion) {
+		sesion.setAttribute("pedidos", pedidos);
 		addStock();
 		System.out.println(stock.getAll());
 		modelo.addAttribute("stock", stock.getAll());
@@ -27,14 +33,68 @@ public class ControladorCarrito {
 	@PostMapping("/")
 	public String principalPost(@RequestParam(name="manzanas", required=false)Integer manzanas,
 			@RequestParam(name="melones", required=false)Integer melones,
-			@RequestParam(name="peras", required=false)Integer peras, Model modelo) {
+			@RequestParam(name="peras", required=false)Integer peras, 
+			Model modelo,
+			HttpSession sesion) {
+		if (sesion.getAttribute("manzanas")!=null) {
+			sesion.setAttribute("manzanas", manzanas+(Integer)sesion.getAttribute("manzanas"));
+		} else {
+			sesion.setAttribute("manzanas", manzanas);
+		}
+		if (sesion.getAttribute("peras")!=null) {
+			sesion.setAttribute("peras", peras+Integer.parseInt((String) sesion.getAttribute("peras")));
+		} else {
+			sesion.setAttribute("peras", peras);
+		}
+		if (sesion.getAttribute("melones")!=null) {
+			sesion.setAttribute("melones", melones+Integer.parseInt((String) sesion.getAttribute("melones")));
+		} else {
+			sesion.setAttribute("melones", melones);
+		}
 		
-		if (manzanas != null) stock.modificar("manzanas", stock.getAll().get("manzanas")-manzanas);
+		Integer total=0;
 		
-		if (peras != null) stock.modificar("peras", stock.getAll().get("peras")-peras);
+		if (manzanas != null) {
+			
+			if (manzanas > 0) stock.modificar("Manzanas", stock.getAll().get("Manzanas")-manzanas);
+			
+			if (manzanas > stock.getAll().get("Manzanas")) {
+				pedidos.put("Manzanas", stock.getAll().get("Manzanas"));
+			} else {
+				pedidos.put("Manzanas", (Integer)sesion.getAttribute("manzanas"));
+			}
+			total+=pedidos.get("Manzanas");
+		}
 		
-		if (melones != null) stock.modificar("melones", stock.getAll().get("melones")-melones);
+		if (peras != null) {
+			
+			if (peras > 0) stock.modificar("Peras", stock.getAll().get("Peras")-peras);
+			
+			if (peras > stock.getAll().get("Peras")) {
+				pedidos.put("Peras", stock.getAll().get("Peras"));
+			} else {
+				pedidos.put("Manzanas", Integer.parseInt((String) sesion.getAttribute("peras")));
+			}
+			total+=pedidos.get("Peras");
+		}
 		
+		if (melones != null) {
+			
+			if (melones > 0) stock.modificar("Melones", stock.getAll().get("Melones")-melones);
+			
+			if (melones > stock.getAll().get("Melones")) {
+				pedidos.put("Melones", stock.getAll().get("Melones"));
+			} else {
+				pedidos.put("Manzanas", Integer.parseInt((String) sesion.getAttribute("melones")));
+			}
+			total+=pedidos.get("Melones");
+		}
+		stock.guardar();
+		
+		modelo.addAttribute("stock", stock.getAll());
+		modelo.addAttribute("productos", pedidos);
+		modelo.addAttribute("total",total);
+
 		return "index";
 	}
 	
